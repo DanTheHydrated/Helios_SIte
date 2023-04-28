e376397a49acef6c1355cd6913cd20197b2b4447
Dan W <daniel.winn@student.awesomeinc.org>
2023-04-28 19:23:49 +0000

    this is the end of the Nav Bar, I spent way too much time on it but I like how it came out.

--- helios/src/Nav.js
+++ helios/src/Nav.js
@@ -1,16 +1,32 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css'; 
 import Logo from "./Photos/HellosLogo.jpg"
+import { useState } from 'react';
 
 
 export default function Nav() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const handleNavToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    };
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container">
                 <a className="navbar-brand" href="/">
                     <img src={Logo} alt="Logo" className="navbar-logo" />
                 </a>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={handleNavToggle}
+                    aria-controls="navbarNav"
+                    aria-expanded={isNavOpen}
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse justify-content-end${isNavOpen ? ' show' : ''}`} id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <a className="nav-link" href="/">Home</a>
